a64acaa1b173c021db83471a08d53561017a01f6
cndesantana <charles.desantana@eawag.ch>
2015-12-22 18:32:38 +0100

    Update the model

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,7 @@ using Dynaland
 #;# To define a way to identify the steady state (based on the ratio between sd(rich)/mean(rich), for example
 #;# To check if the number of species in the steady stae matches with the literature (paper in PRE 2003, that says that we would expect 240 species in our system for steady state)
  
-function main(mode_,nvals_,gmax_)
+function main(mode_,nvals_,gmax_,mr_)
 	
         println("Testing Dynaland.jl");
         mode = parse(Int,mode_);#mode 1 means static landscape, mode 2 means dynamic landscape
@@ -23,7 +23,7 @@ function main(mode_,nvals_,gmax_)
 	landG = 1;#Number of generations before a landscape upgrade
 	S = 100;#Number of sites
 	J = 100;#Number of inds. per site
-	mr = 0.3;#migration between sites 
+	mr = parse(Float64,mr_);#migration between sites 
 	vr = 0.003;#migration from the regional pool (speciation)
         sufix = "Static";
         if(mode == 2)
@@ -42,8 +42,8 @@ function main(mode_,nvals_,gmax_)
 end
 
 if(length(ARGS) != 3)
-   println("\nTo run: julia runtest.jl MODE NVALS GMAX\n\n-MODE:\t1 (Static) or 2 (Dynamic)\n-NVALS:\tNumber of values for each parameter\n-GMAX:\tNumber of Generations to run\n");
+   println("\nTo run: julia runtest.jl MODE NVALS GMAX\n\n-MODE:\t1 (Static) or 2 (Dynamic)\n-NVALS:\tNumber of values for each parameter\n-GMAX:\tNumber of Generations to run\n-mr: Migration rate\n");
    return;
 else
-   main(ARGS[1],ARGS[2],ARGS[3]);
+   main(ARGS[1],ARGS[2],ARGS[3],ARGS[4]);
 end
